91b85d429d1b492c15c6623393edcb0e719e5425
Érik Freitas Morais <74334533+erikfritas@users.noreply.github.com>
2021-09-23 03:10:20 -0300

    Add files via upload

--- js/img_body.js
+++ js/img_body.js
@@ -3,13 +3,25 @@
  * for the image that is currently displayed.
  */
 
-let st, ed
+let st, ed, src, img
 
-$(window).scroll((e)=>{
+$(window).scroll(()=>{
     st = $(window).scrollTop()
-    ed = $("img").offset().top
+    
+    img = 'img.bg'
+    ed = ($(img).offset().top - st) + ($(img).height()/2)
+
+    /*
+    for debugs
 
     console.log('\n');
     console.log(`scrollTop: ${st}`)
     console.log(`element distacy of top: ${ed}`)
+    */
+
+    if (ed >= 0){
+        src = $(img).attr('src')
+        $('div.body')
+        .css('background-image', `url(${src})`)
+    }
 })
